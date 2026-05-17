444d044184a7ff8113afa52afa21a583717894ed
Turzz <gunturzz2012@gmail.com>
2026-05-17 22:11:51 +0700

    Update tokens - 2026-05-17T15:11:51.112Z

--- token.js
+++ token.js
@@ -1,6 +1,7 @@
 {
   "tokens": [
     "8638035357:AAEP7eB6kSbGbhbBvaEJrn8AbIEP3aGPyaY",
-    "8686198153:AAELETTCnSbkPL-hmBsY9DqJt5c9N6f6rIY"
+    "8686198153:AAELETTCnSbkPL-hmBsY9DqJt5c9N6f6rIY",
+    "8972182576:AAGxaMrfansPXBoQvvepu--RJTQWGvzYTO8"
   ]
 }
